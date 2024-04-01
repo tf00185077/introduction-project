9d0e6f08d0c294bdad626620cd5105283eb71e3e
tf00185077 <tf00185077@gmail.com>
2024-04-01 23:46:53 +0800

    sun css

--- components/defaultLayout/Header.vue
+++ components/defaultLayout/Header.vue
@@ -4,8 +4,28 @@
             <li>Introduction</li>
             <li>Side Project</li>
         </ul>
+        <SwitchButtonDarkAndLightThene></SwitchButtonDarkAndLightThene>
     </div>
 </template>
 <style lang="scss" scoped>
-@import url('assets/css/Layout/header.scss');
+.header-outline{
+    box-sizing: border-box;
+    width:100%;
+    border-bottom:1px solid var(--border-color);
+    padding:12px 24px;
+    display:flex;
+    justify-content: space-between;
+    ul{
+        padding:0;
+        margin:0;
+        display:flex;
+        align-items: center;
+        gap:24px;
+        list-style: none;
+        color:var(--primary-text-color);
+        li{
+            cursor:pointer;
+        }
+    }
+}
 </style>
---
+++ components/switchButton/DarkAndLightThene.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="btn-outline">
+        <div class="btn-move-outline">
+            <div class="sun"></div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.btn-outline{
+    border: 1px solid var(--border-color);
+    min-width:60px;
+    height:12px;
+    border-radius: 24px;
+    padding:14px 6px;
+    display:flex;
+    align-items: center;
+
+}
+.btn-move-outline{
+    width:100%;
+    height:2px;
+    background-color: var(--primary-text-color);
+    position:relative;
+    .sun{
+        cursor: pointer;
+        position:absolute;
+        top:50%;
+        left:0;
+        transform:translateY(-50%);
+        width:50%;
+        padding-top:50%;
+        background-color: #fff;
+        border-radius: 50%;
+        background-color: #FFD580 ;
+        box-shadow: 0 0 10px #FFD580;
+    }
+}
+</style>
