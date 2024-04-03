3010600e85adce7aadd147d11416a25f648d1737
tf00185077 <tf00185077@gmail.com>
2024-04-03 22:11:43 +0800

    fix->css position

--- components/IndexBg.vue
+++ components/IndexBg.vue
@@ -3,8 +3,10 @@
 </template>
 <style scoped lang="scss">
 .bg {
+  z-index: -1;
   position: absolute;
   top: 0;
+  left: 0;
   background-image: url("/assets/gif/landing-page.gif");
   width: 100%;
   height: 100%;
