34c829d58343c1779a05b74267c394ee2641d982
tf00185077 <tf00185077@gmail.com>
2024-04-03 21:47:55 +0800

    bg

---
+++ components/IndexBg.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg"></div>
+</template>
+<style scoped lang="scss">
+.bg {
+  position: absolute;
+  top: 0;
+  background-image: url("/assets/gif/landing-page.gif");
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+  opacity: 1;
+  animation: opacityAnime 3s infinite;
+}
+@keyframes opacityAnime {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.4;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
