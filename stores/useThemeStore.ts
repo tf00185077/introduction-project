f9a707b0b286e9a005f1e29703f4e9320772e306
tf00185077 <tf00185077@gmail.com>
2024-04-11 23:40:45 +0800

    add variable

--- stores/useThemeStore.ts
+++ stores/useThemeStore.ts
@@ -4,9 +4,13 @@ export const useThemeStore = defineStore("themeStore", () => {
     "--background-color": string;
     "--primary-text-color": string;
     "--secondary-text-color": string;
+    "--primary-text-bg-color": string;
     "--primary-accent-color": string;
     "--secondary-accent-color": string;
     "--border-color": string;
+    "--btn-bg-color": string;
+    "--btn-text-color": string;
+    "--btn-border-color": string;
   };
   const mode = ref<string|number>('dark');
   // 定义一个包含所有主题和对应颜色的对象类型
@@ -19,17 +23,25 @@ export const useThemeStore = defineStore("themeStore", () => {
       "--background-color": "#FFFFFF",
       "--primary-text-color": "#333333 ",
       "--secondary-text-color": "#757575",
+      "--primary-text-bg-color":"#E8F5E9",
       "--primary-accent-color": "#018786 ",
       "--secondary-accent-color": "#6200EE",
       "--border-color": "#BDBDBD",
+      "--btn-bg-color":"transparnent",
+      "--btn-text-color":"#FFFFFF",
+      "--btn-border-color":"#009688"
     },
     dark: {
       "--background-color": "#000",
       "--primary-text-color": "#E0E0E0",
       "--secondary-text-color": "#BDBDBD",
+      "--primary-text-bg-color":"#263238",
       "--primary-accent-color": "#03DAC5",
       "--secondary-accent-color": "#BB86FC",
       "--border-color": "#BDBDBD ",
+      "--btn-bg-color":"transparnent",
+      "--btn-text-color":"#00363a",
+      "--btn-border-color":"#018786"
     },
   };
 
@@ -39,7 +51,6 @@ export const useThemeStore = defineStore("themeStore", () => {
     localStorage.setItem('theme',themeName.toString())
     const root = document.documentElement;
 
-    // 使用 Object.entries 来确保键值对的类型安全
     Object.entries(theme).forEach(([property, value]) => {
       root.style.setProperty(property, value);
     });
