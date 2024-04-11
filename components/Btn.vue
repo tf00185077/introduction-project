c43bade047672e3184f821dd7b1c50d4c6d2e9b2
tf00185077 <tf00185077@gmail.com>
2024-04-11 23:40:01 +0800

    btn component

---
+++ components/Btn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="custom-component-btn"><slot /></div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="scss">
+.custom-component-btn {
+  background-color: var(--btn-bg-color);
+  color: var(--primary-text-color);
+  box-shadow:0 0 5px var(--secondary-accent-color);
+  border: 2px solid var(--border-color);
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  user-select: none;
+  &:active {
+    transform: translate(0, 2px);
+  }
+}
+</style>
