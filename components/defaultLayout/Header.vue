d70889ccd2d933f064a895067b25c33f22bfc760
redgodkill <tf00185077@gmail.com>
2024-04-11 10:54:51 +0800

    delete useless keepalive

--- components/defaultLayout/Header.vue
+++ components/defaultLayout/Header.vue
@@ -1,22 +1,23 @@
 <template>
-  <KeepAlive>
-    <div class="header-outline">
-      <ul>
-        <li v-for="page in pageList">
-          <input type="radio" v-model="chooseHeader" :id="'/'+page" :value="'/' + page" /><label
-            :for="'/' + page"
-            @click="navigateTo('/' + page)"
-            >{{ page }}</label
-          >
-        </li>
-      </ul>
-      <SwitchButtonDarkAndLightThene></SwitchButtonDarkAndLightThene>
-    </div>
-  </KeepAlive>
+  <div class="header-outline">
+    <ul>
+      <li v-for="page in pageList">
+        <input
+          type="radio"
+          v-model="chooseHeader"
+          :id="'/' + page"
+          :value="'/' + page"
+        /><label :for="'/' + page" @click="navigateTo('/' + page)">{{
+          page
+        }}</label>
+      </li>
+    </ul>
+    <SwitchButtonDarkAndLightThene></SwitchButtonDarkAndLightThene>
+  </div>
 </template>
 <script lang="ts" setup>
 const chooseHeader = ref(useRoute().path);
-const {data:pageList,refresh} = await useFetch("/api/getPages")
+const { data: pageList, refresh } = await useFetch("/api/getPages");
 </script>
 <style lang="scss" scoped>
 .header-outline {
